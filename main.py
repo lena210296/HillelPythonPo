97fc969e44a549990711a3d1c7bbbe7fe79600f6
[Olena Karpenko] <[lena.k.21.02@gmail.com]>
2023-05-18 00:42:09 +0200

    final

--- main.py
+++ main.py
@@ -8,13 +8,6 @@ def parse_cookie(query: str) -> dict:
     return output
 
 
-test = parse_cookie(
-    'devicePixelRatio=1;ident=exists;__utma=13103r6942.2918;t_session'
-    '=BAh7DUkiD3Nlc3NpbWVfZV9uYW1lBjsARkkiH1BhY2lmaWMgVGltZSAoVVMgJiBDYW5hZGEpBjsAVEkiFXNpZ25pbl9wZXJzb25faWQGOwBGaQMSvR'
-    'pJIhRsYXN0X2xvZ2luX2RhdGUGOwBGVTogQWN0aXZlU3VwcG9ydDo6VGltZVdpdGhab25lWwhJdToJVGltZQ2T3RzAAABA7QY6CXpvbmVJIghVVEMGO'
-    'wBUSSIfUGFjaWZpZWRfZGFzaGJvYXJkX21lc3NhZ2UGOwBGVA%3D%3D--6ce6ef4bd6bc1a469164b6740e7571c754b31cca')
-test
-
 if __name__ == '__main__':
     assert parse_cookie('name=Dima;') == {'name': 'Dima'}
     assert parse_cookie('') == {}

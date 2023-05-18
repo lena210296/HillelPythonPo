c553bd08a3fd40977b3c374e9fa4b95067d3d673
[Olena Karpenko] <[lena.k.21.02@gmail.com]>
2023-05-18 14:36:18 +0200

    changes

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     assert parse_cookie(
         'devicePixelRatio=1; ident=exists; '
         '__utma=13103r6942.2918;t_session'
-        '=BAh7DUkiD3Nlc3NpbWVfZV9uYW1lBjsARkkiH1BhY2lmaWMgVGltZSAoVVMgJiBDYW5hZGEpBjsAVEkiFXNpZ25pbl9wZXJzb25faWQGOwBGaQ'
+        '=BAh7DUkiD3Nlc3NpbWVfZV9uYW1lBjsARkkiH1BhY2lmaWMgVGltZSAoVVMgJiBDYW5hZgit GEpBjsAVEkiFXNpZ25pbl9wZXJzb25faWQGOwBGaQ'
         'MSvRpJIhRsYXN0X2xvZ2luX2RhdGUGOwBGVTogQWN0aXZlU3VwcG9ydDo6VGltZVdpdGhab25lWwhJdToJVGltZQ2T3RzAAABA7QY6CXpvbmVJI'
         'ghVVEMGOwBUSSIfUGFjaWZpZWRfZGFzaGJvYXJkX21lc3NhZ2UGOwBGVA%3D%3D--6ce6ef4bd6bc1a469164b6740e7571c754b31cca') == {
                'devicePixelRatio': '1',
